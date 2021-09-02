c1cd1c0dc09c82fec3b350b222987f31e4dcb8cf
richardingwe <richardingwe5@gmail.com>
2021-09-02 22:19:10 +0100

    updated alg1

--- alg1.js
+++ alg1.js
@@ -1,3 +1,11 @@
+/** write a function that takes two string arrays and adds the numbers with the same index from both arrays together
+    example: ['[1,2,3,9,2]', '[2,4,5,10,7,55,0]'] would return ['(1 + 2)', '(2 + 4)', '(3 + 5)'],
+    if the second array is has more numbers than the first one, then return the remaining numbers as they were.
+    the function should return its result as 3-6-8-19-9-55-0
+ **/
+
+
+
 function strrArr(myArr) {
     const arr1 = JSON.parse(myArr[0]);
     const arr2 = JSON.parse(myArr[1]);
